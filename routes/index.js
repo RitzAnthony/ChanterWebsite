c59a0c76bedc693db80c5cb217b6161d8dac46a4
RitzAnthony <anthony.ritz@students.hevs.ch>
2018-10-13 12:15:50 +0200

    enabled debugging

--- routes/index.js
+++ routes/index.js
@@ -42,6 +42,7 @@ exports = module.exports = function (app) {
 	app.get('/events', routes.views.events);
 	app.get('/choir', routes.views.choir);
 	app.all('/contact', routes.views.contact);
+	
 
 	// NOTE: To protect a route so that only admins can see it, use the requireUser middleware:
 	// app.get('/protected', middleware.requireUser, routes.views.protected);
