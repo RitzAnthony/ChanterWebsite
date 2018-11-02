a37f61d68aadd5a39bd0d82843c5b5d09e450004
Anthony <anthony.ritz@gmx.ch>
2018-11-02 11:23:22 +0100

    first implementation of dynamic dropdowns (not stable)

---
+++ models/Dropdown.js
@@ -0,0 +1,21 @@
+var keystone = require('keystone');
+var Types = keystone.Field.Types;
+
+/**
+ * Dropdown Model
+ * ==========
+ */
+
+
+var Dropdown = new keystone.List('Dropdown');
+
+Dropdown.add({
+	name: {type: String, required: true, index: true}});
+
+
+
+/**
+ * Registration
+ */
+Dropdown.defaultColumns = 'name';
+Dropdown.register();
